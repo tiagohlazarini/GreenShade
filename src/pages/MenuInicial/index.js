4eb448cbffc877523699e8cb19a4ee22892577ad
Ricardo Leme <ricardo.leme@gmail.com>
2020-10-07 12:24:44 -0300

    Ajustes no Menu

--- src/pages/MenuInicial/index.js
+++ src/pages/MenuInicial/index.js
@@ -1,17 +1,20 @@
+//cSpell:Ignore Cabecalho
 import React from 'react';
+
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+
 import PersonPinIcon from '@material-ui/icons/PersonPin'
 import HelpIcon from '@material-ui/icons/Help'
 import AccessibilityIcon from '@material-ui/icons/Accessibility';
 import LocalOfferIcon from '@material-ui/icons/LocalOffer'
 
 import Cabecalho from '../Cabecalho'
+import Tarefas from '../Tarefas'
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -26,8 +29,10 @@ function TabPanel(props) {
         >
             {value === index && (
                 <Box p={3}>
-                    <Typography>{children}</Typography>
+                    {/* Remover o Typography do Exemplo */}
+                    {children}
                 </Box>
+
             )}
         </div>
     );
@@ -66,23 +71,23 @@ export default function SimpleTabs() {
             <Cabecalho />
             <AppBar position="static">
                 <Tabs value={value} onChange={handleChange} centered aria-label="Opções do Menu">
-                    <Tab label="Produtos" icon={<LocalOfferIcon />} {...a11yProps(0)} />
+                    <Tab label="Tarefas" icon={<LocalOfferIcon />} {...a11yProps(0)} />
                     <Tab label="Usuários" icon={<PersonPinIcon />} {...a11yProps(1)} />
                     <Tab label="Clientes" icon={<AccessibilityIcon />} {...a11yProps(2)} />
                     <Tab label="Ajuda" icon={<HelpIcon />} {...a11yProps(3)} />
                 </Tabs>
             </AppBar>
             <TabPanel value={value} index={0}>
-                Produtos
+                <Tarefas />
       </TabPanel>
             <TabPanel value={value} index={1}>
-                Usuários
+                
       </TabPanel>
             <TabPanel value={value} index={2}>
-                Clientes
+                
       </TabPanel>
             <TabPanel value={value} index={3}>
-                Ajuda
+                
       </TabPanel>
         </div>
     );
