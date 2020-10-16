3e98c3d7627906e488d99ea480d5d1e70f1a4c32
Ricardo Leme <ricardo.leme@gmail.com>
2020-10-16 14:21:42 -0300

    Add HashRouter

--- src/rotas.js
+++ src/rotas.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom"
+import { HashRouter, Switch, Route } from "react-router-dom"
 import Login from "./pages/Login"
 import Inicio from "./pages/Inicio"
 import NaoEncontrado from "./pages/NaoEncontrado"
@@ -9,13 +9,13 @@ import RotasPrivadas from './rotasPrivadas'
 
 export default function Rotas(){
   return(
-<BrowserRouter>
+<HashRouter>
   <Switch>
-    <Route exact path={process.env.PUBLIC_URL + '/'} component={Inicio} />
-    <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
-    <RotasPrivadas exact path={process.env.PUBLIC_URL + '/menu'} component={MenuInicial} />
+    <Route exact path='/' component={Inicio} />
+    <Route exact path='/login' component={Login} />
+    <RotasPrivadas exact path='/menu' component={MenuInicial} />
     <Route component={NaoEncontrado} />
   </Switch>
-</BrowserRouter>
+</HashRouter>
   )
 }
