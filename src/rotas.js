0a32dc77a5787b9eb07026c2eb2e4a1f91f8a820
Ricardo Leme <ricardo.leme@gmail.com>
2020-10-16 14:06:47 -0300

    process.env nas rotas

--- src/rotas.js
+++ src/rotas.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Switch, Route } from "react-router-dom"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
 import Login from "./pages/Login"
 import Inicio from "./pages/Inicio"
 import NaoEncontrado from "./pages/NaoEncontrado"
@@ -9,13 +9,13 @@ import RotasPrivadas from './rotasPrivadas'
 
 export default function Rotas(){
   return(
-<HashRouter basename={process.env.PUBLIC_URL}>
+<BrowserRouter>
   <Switch>
     <Route exact path={process.env.PUBLIC_URL + '/'} component={Inicio} />
-    <Route exact path="/login" component={Login} />
-    <RotasPrivadas exact path="/menu" component={MenuInicial} />
+    <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
+    <RotasPrivadas exact path={process.env.PUBLIC_URL + '/menu'} component={MenuInicial} />
     <Route component={NaoEncontrado} />
   </Switch>
-</HashRouter>
+</BrowserRouter>
   )
 }
